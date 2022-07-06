7ae17c9defc8d761306e39512aee9d2f6582b3d3
creaspo <creaspo23@gmail.com>
2022-07-06 13:48:58 +0200

    :fire: installed eslint and config it

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 const Lol = ({ name, animal, breed }) => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, name),
-            React.createElement("h2", {}, animal),
-            React.createElement("h2", {}, breed),
-
-        ]
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
 };
-
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me !"),
-            React.createElement(Lol, { name: "ali", animal: "x", breed: "m" }),
-            React.createElement(Lol, { name: "omer", animal: "y", breed: "f" }),
-            React.createElement(Lol, { name: "ahmed", animal: "z", breed: "m" }),
-        ]
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me !"),
+    React.createElement(Lol, { name: "ali", animal: "x", breed: "m" }),
+    React.createElement(Lol, { name: "omer", animal: "y", breed: "f" }),
+    React.createElement(Lol, { name: "ahmed", animal: "z", breed: "m" }),
+  ]);
 };
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById("root"),
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
