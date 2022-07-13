56fc181dba8b0ae06bdef3a88b41b07dac206d98
creaspo <creaspo23@gmail.com>
2022-07-13 12:05:04 +0200

    :fire: added options and pervent api

--- src/serachPramas.js
+++ src/serachPramas.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-
+import { ANIMALS } from "@frontendmasters/pet";
 const SearchPramas = () => {
 
     const [location, setLocation] = useState('Seattle,WA');
+    const [animal, setAnimal] = useState("dog");
 
     return (
         <div className="search-params">
@@ -10,8 +11,25 @@ const SearchPramas = () => {
                 <label htmlFor="location">
                     location
                     <input id="location" value={location} placeholder="location" onChange={e => setLocation(e.target.value)} />
+                    <label htmlFor="animal">
+                    Animal
+                    <select id="animal"
+                        value={animal}
+                        onChange={e => setAnimal(e.target.value)}
+                        onBlur={e => setAnimal(e.target.value)}
+
+                    >
+                        <option value="" key="">All</option>
+                        {ANIMALS.map(animal => (
+                            <option value={animal}>{animal}</option>
+                        ))}
+
+                    </select>
+                </label>
                     <button>submit</button>
                 </label>
+
+          
             </form>
         </div>
     );
